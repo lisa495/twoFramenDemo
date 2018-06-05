d4b14ad65133c3040d4362d1cc03470e7a8de250
zhaoyk1816 <zhaoyongkang1816@163.com>
2018-06-05 18:27:23 +0800

    全局参数配置

--- app/src/main/java/com/basicframe/demo/MainActivity.java
+++ app/src/main/java/com/basicframe/demo/MainActivity.java
@@ -1,11 +1,18 @@
 package com.basicframe.demo;
 
+import android.content.Intent;
 import android.view.View;
 import com.base.framework.activity.ReBaseActivity;
+import com.base.framework.presenter.BasePresenter;
 import com.base.framework.viewWiget.common.TopBarView;
 
-public abstract class MainActivity extends ReBaseActivity {
+public  class MainActivity extends ReBaseActivity {
 
+
+    @Override
+    protected void beforeInitView(Intent intent) {
+
+    }
 
     /**
      *  默认处理一些，具体根据自己当前设置,重写这个{@link #initTopBarView(TopBarView)}方法
@@ -24,6 +31,32 @@ public abstract class MainActivity extends ReBaseActivity {
         initTopBarView(topBarView);
         return topBarView;
     }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getContentResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initContentView(View contentView) {
+
+    }
+
+    @Override
+    protected void loadData(BasePresenter mPresenter) {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
     //初始化topBarView，
     protected void initTopBarView(TopBarView topBarView){}
 
